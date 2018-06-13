b0a537b835a91ba9aade59ec1319fec1619d21c0
KrishnaRaj <Krishna.Devarajan-Nair@saiglobal.com>
2018-06-13 16:54:02 +1000

    initial commit

---
+++ App/App/Models/MainMenuItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace App.Models
+{
+    public class MainMenuItems
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public dynamic TargetType { get; set; }
+    }
+}
--- App/App/Views/MainPage.xaml.cs
+++ App/App/Views/MainPage.xaml.cs
@@ -1,16 +1,49 @@
-﻿using System;
-
+﻿using App.Models;
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
-	{
-		public MainPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MainMenuItems> MainMenuItems { get; set; }
+        public MainPage()
+        {
+            BindingContext = this;
+
+            // Build the Menu
+            MainMenuItems = new List<MainMenuItems>
+            {
+               new MainMenuItems() { Title = "About", Icon = "menu_about.png", TargetType = typeof(About) },
+               new MainMenuItems() { Title = "Contact", Icon = "menu_contact.png", TargetType = typeof(Contact) }
+            };
+
+            // Set the default page, this is the "home" page.
+            Detail = new NavigationPage(new About());
+
+            InitializeComponent();
+        }
+
+        private void MenuListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MainMenuItems;
+            if (item != null)
+            {
+                if (item.Title.Equals("About"))
+                {
+                    Detail = new NavigationPage(new About());
+                }
+                else if (item.Title.Equals("Contact"))
+                {
+                    Detail = new NavigationPage(new Contact());
+                }
+
+                MenuListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
 }
